421a31a69b8cb83c171a0f5859395e473e6df2cc
KrumIliev <krumiliev@yahoo.com>
2015-03-07 13:56:59 +0200

    Fixed IndexOutOfBounds with screen pixels array

--- src/com/kdi/dungeons/Game.java
+++ src/com/kdi/dungeons/Game.java
@@ -1,7 +1,6 @@
 package com.kdi.dungeons;
 
 import java.awt.Canvas;
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
--- src/com/kdi/dungeons/graphics/Screen.java
+++ src/com/kdi/dungeons/graphics/Screen.java
@@ -5,7 +5,7 @@ public class Screen {
 	private int width, height;
 	public int[] pixels;
 
-	int time = 0; // For testing
+	int xTime = 0, yTime; // For testing
 	int counter = 0; // For testing
 
 	public Screen(int width, int height) {
@@ -21,11 +21,14 @@ public class Screen {
 
 	public void render() {
 		counter++;
-		if (counter % 100 == 0) time++;
+		if (counter % 10 == 0) xTime++;
+		if (counter % 80 == 0) yTime++;
 
 		for (int y = 0; y < height; y++) {
+			if (yTime >= height) break;
 			for (int x = 0; x < width; x++) {
-				pixels[time + time * width] = 0xff00ff;
+				if (xTime >= width) break;
+				pixels[xTime + yTime * width] = 0xff00ff;
 			}
 		}
 	}
