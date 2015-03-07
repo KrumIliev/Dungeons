d83c32c0bb2441bc4c14d8154a5c67e1f4359813
KrumIliev <krumiliev@yahoo.com>
2015-03-07 10:05:27 +0200

    Initial commit

---
+++ src/com/kdi/dungeons/Game.java
@@ -0,0 +1,104 @@
+package com.kdi.dungeons;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+import com.kdi.dungeons.graphics.Screen;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static int width = 300;
+	public static int height = width / 16 * 9;
+	public static int scale = 3;
+
+	private Thread gameThread;
+	private boolean running = false;
+	private Screen screen;
+
+	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	private int[] pixels = ((DataBufferInt) image.getRaster().getDataBuffer()).getData();
+
+	private JFrame frame;
+
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+
+		screen = new Screen(width, height);
+		frame = new JFrame();
+	}
+
+	public synchronized void start() {
+		running = true;
+		gameThread = new Thread(this);
+		gameThread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (running) {
+			update();
+			render();
+		}
+	}
+
+	public void update() {
+
+	}
+
+	public void render() {
+		BufferStrategy bufferStrategy = getBufferStrategy();
+		if (bufferStrategy == null) {
+			createBufferStrategy(3);
+			return;
+		}
+
+		screen.render();
+
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
+
+		Graphics graphics = bufferStrategy.getDrawGraphics();
+		// Draw all graphics between creating the graphics and disposing ------------>
+
+		graphics.drawImage(image, 0, 0, getWidth(), getHeight(), null);
+
+		// <--------------------------------------------------------------------------
+		graphics.dispose();
+		bufferStrategy.show();
+
+	}
+
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.frame.setResizable(false);
+		game.frame.setTitle("Dungeons");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+
+		game.start();
+	}
+
+}
