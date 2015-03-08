fecd4f7f67032d09c63e461277604625dbd0587b
KrumIliev <krumiliev@yahoo.com>
2015-03-09 00:17:46 +0200

    Changed screen from 16/9 to 4/3

--- src/com/kdi/dungeons/Game.java
+++ src/com/kdi/dungeons/Game.java
@@ -20,7 +20,7 @@ public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 
 	public static int width = 300;
-	public static int height = width / 16 * 9;
+	public static int height = width / 4 * 3;
 	public static int scale = 3;
 
 	private Thread gameThread;
